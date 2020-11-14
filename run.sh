5e584ee3390b11dbfa8b1e3d5e0bb76275860799
Jorge Brown <jorge.pc.brown@gmail.com>
2020-11-14 13:26:13 +0000

    All done up to 2 g)

--- run.sh
+++ run.sh
@@ -12,14 +12,23 @@ for i in tests/*.txt; do
     fstcompile --isymbols=syms.txt --acceptor $i | fstarcsort > compiled/$(basename $i ".txt").test.fst
 done
 
+echo "Creating text2num using horas, e, minutos transducers"
+fstconcat compiled/horas.source.fst compiled/e.source.fst aux.source.fst
+fstconcat aux.source.fst compiled/minutos.source.fst compiled/text2num.source.fst
+rm aux.source.fst
 
-# TODO
+echo "Creating lazy2num using horas, e, lazye, minutos transducers"
+fstconcat compiled/e.source.fst compiled/minutos.source.fst auxminutos.source.fst
+fstunion auxminutos.source.fst compiled/lazye.source.fst auxminutos.source.fst
+fstconcat compiled/horas.source.fst auxminutos.source.fst compiled/lazy2num.source.fst
+rm auxminutos.source.fst
 
+echo "Creating rich2text using skiptexthoras, skipe, quartos transducers"
+fstconcat compiled/skiptexthoras.source.fst compiled/skipe.source.fst aux.source.fst
+fstconcat aux.source.fst compiled/quartos.source.fst compiled/rich2text.source.fst
+rm aux.source.fst
 
 for i in compiled/*.source.fst; do
 	echo "Creating image: images/$(basename $i '.fst').pdf"
     fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
 done
-
-#echo "Testing the transducer 'horas' with the input 'tests/numero.txt'"
-#fstcompose compiled/numero.fst compiled/horas.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt --osymbols=./syms.txt
